68a73604e135c72a0eb86ca94a749d6f6b91590a
Bela Vander Voort <bvandervoort@insitesoft.com>
2017-12-14 13:27:16 -0600

    Make sure building extension will work with newest Extensions.csproj file

--- dist/buildExtensions.ps1
+++ dist/buildExtensions.ps1
@@ -1,3 +1,10 @@
 Import-Module -Name ($PSScriptRoot + "\Invoke-MsBuild.psm1")
 Invoke-MsBuild -Path ($PSScriptRoot + "\..\src\Extensions\Extensions.csproj") -MsBuildParameters "/t:Build /p:Configuration=Release"
-Copy-Item -Path ($PSScriptRoot + "\..\src\Extensions\bin\Release\Extensions.dll") -Destination $PSScriptRoot
+if (Test-Path $PSScriptRoot + "\..\src\Extensions\bin\Release\Extensions.dll")
+{ 
+    Copy-Item -Path ($PSScriptRoot + "\..\src\Extensions\bin\Release\Extensions.dll") -Destination $PSScriptRoot
+}
+else 
+{
+    Copy-Item -Path ($PSScriptRoot + "\..\src\Extensions\bin\Extensions.dll") -Destination $PSScriptRoot
+}
