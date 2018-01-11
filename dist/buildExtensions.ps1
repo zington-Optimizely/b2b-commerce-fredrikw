916bee3869362d6e1dc9e4b1c1f97ef4d326f202
etoropov <30478386+etoropov@users.noreply.github.com>
2018-01-11 19:39:39 +0300

    ISC-6821 Fix buildExtensions.ps1 script for cloud
    (wrap with brackets)

--- dist/buildExtensions.ps1
+++ dist/buildExtensions.ps1
@@ -1,6 +1,6 @@
 Import-Module -Name ($PSScriptRoot + "\Invoke-MsBuild.psm1")
 Invoke-MsBuild -Path ($PSScriptRoot + "\..\src\Extensions\Extensions.csproj") -MsBuildParameters "/t:Build /p:Configuration=Release"
-if (Test-Path $PSScriptRoot + "\..\src\Extensions\bin\Release\Extensions.dll")
+if (Test-Path ($PSScriptRoot + "\..\src\Extensions\bin\Release\Extensions.dll"))
 { 
     Copy-Item -Path ($PSScriptRoot + "\..\src\Extensions\bin\Release\Extensions.dll") -Destination $PSScriptRoot
 }
