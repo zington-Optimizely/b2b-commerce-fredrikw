bc22a7b8973c652475f1af357e793553f14b4d0c
IS\jrobertson <jrobertson@insitesoft.com>
2018-01-24 11:07:54 -0600

    4.4.0.38656

--- src/InsiteCommerce.Web/Properties/AssemblyInfo.cs
+++ src/InsiteCommerce.Web/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.4.0.38481")]
-[assembly: AssemblyFileVersion("4.4.0.38481")]
+[assembly: AssemblyVersion("4.4.0.38656")]
+[assembly: AssemblyFileVersion("4.4.0.38656")]
 
