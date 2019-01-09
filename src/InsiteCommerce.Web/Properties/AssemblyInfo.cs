9847c47edc4c079ffe3b70e63a754998ffdb3f2c
Peter Gaard <pgaard@gmail.com>
2019-01-09 13:01:19 -0600

    4.4.10.43817

--- src/InsiteCommerce.Web/Properties/AssemblyInfo.cs
+++ src/InsiteCommerce.Web/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.4.10.43811")]
-[assembly: AssemblyFileVersion("4.4.10.43811")]
+[assembly: AssemblyVersion("4.4.10.43817")]
+[assembly: AssemblyFileVersion("4.4.10.43817")]
 
