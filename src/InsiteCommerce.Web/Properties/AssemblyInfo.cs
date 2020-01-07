de18f5f5317913027f5b9037cbe3773e3928cbd5
Insite <insite@insitesoft.com>
2020-01-07 15:44:43 +0000

    4.5.2.53672

--- src/InsiteCommerce.Web/Properties/AssemblyInfo.cs
+++ src/InsiteCommerce.Web/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Insite Software")]
 [assembly: AssemblyProduct("InsiteCommerce")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.5.2.53247")]
-[assembly: AssemblyFileVersion("4.5.2.53247")]
+[assembly: AssemblyVersion("4.5.2.53672")]
+[assembly: AssemblyFileVersion("4.5.2.53672")]
 
