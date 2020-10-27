56483ff17ce703d7adb9b6c5333b4fb97a6f21c7
Insite <insite@insitesoft.com>
2020-10-27 18:58:24 +0000

    5.0.6.103

--- src/FrontEnd/modules/content-library/src/Widgets/ProductDetails/ProductDetailsSpecifications.tsx
+++ src/FrontEnd/modules/content-library/src/Widgets/ProductDetails/ProductDetailsSpecifications.tsx
@@ -21,6 +21,8 @@ import { css } from "styled-components";
 const enum fields {
     showDocuments = "showDocuments",
     showAttributes = "showAttributes",
+    attributesPosition = "attributesPosition",
+    documentsPosition = "documentsPosition",
 }
 
 type Props = WidgetProps & HasProduct;
@@ -66,13 +68,72 @@ const styles = specificationsStyles;
 
 const ProductDetailsSpecifications: React.FC<Props> = ({
     product: { brand, attributeTypes, documents, specifications },
-    fields: { showDocuments, showAttributes },
+    fields: { showDocuments, showAttributes, documentsPosition, attributesPosition },
 }) => {
     const renderingDocuments = showDocuments && documents && documents.length > 0;
-    const renderingAttributes = showAttributes && (brand || attributeTypes);
-    if (!renderingDocuments && !renderingAttributes && !specifications?.length) {
+    const renderingAttributes = showAttributes && (brand || (attributeTypes && attributeTypes?.length > 0));
+    const renderingSpecifications = specifications && specifications.length > 0;
+    const documentsFirst = documentsPosition === "first";
+    const attributesFirst = attributesPosition === "first";
+
+    if (!renderingDocuments && !renderingAttributes && !renderingSpecifications) {
         return null;
     }
+
+    const documentsAccordion = renderingDocuments && (
+        <ManagedAccordionSection
+            title={translate("Documents")}
+            data-test-selector="productDetails_specifications_documents"
+            initialExpanded={
+                ((!attributesFirst || !renderingAttributes) && documentsFirst) ||
+                (!renderingSpecifications && !renderingAttributes)
+            }
+            {...styles.specificationsAccordionSection}
+        >
+            <GridContainer {...styles.documentsContainer}>
+                {documents!.map(document => (
+                    <GridItem key={document.id.toString()} {...styles.documentGridItem}>
+                        <Link
+                            href={document.filePath}
+                            target="_new"
+                            {...styles.documentLink}
+                            data-test-selector={`productDetails_${document.id}`}
+                        >
+                            <IconMemo {...styles.documentIcon} />
+                            <Typography {...styles.documentNameText}>{document.name}</Typography>
+                        </Link>
+                    </GridItem>
+                ))}
+            </GridContainer>
+        </ManagedAccordionSection>
+    );
+
+    const attributesAccordion = renderingAttributes && (
+        <ManagedAccordionSection
+            title={translate("Attributes")}
+            initialExpanded={attributesFirst || (!renderingSpecifications && !renderingDocuments)}
+            data-test-selector="productDetails_specifications_attributes"
+            {...styles.specificationsAccordionSection}
+        >
+            {brand && (
+                <StyledWrapper {...styles.attributeTypeWrapper}>
+                    <Typography {...styles.attributeTypeLabelText}>{translate("Brand")}:&nbsp;</Typography>
+                    <Typography {...styles.attributeTypeValuesText}>{brand.name}</Typography>
+                </StyledWrapper>
+            )}
+            {attributeTypes &&
+                attributeTypes.map(attributeType => (
+                    <StyledWrapper {...styles.attributeTypeWrapper} key={attributeType.id.toString()}>
+                        <Typography {...styles.attributeTypeLabelText}>{attributeType.label}:&nbsp;</Typography>
+                        <Typography {...styles.attributeTypeValuesText}>
+                            {attributeType
+                                .attributeValues!.map(attributeValue => attributeValue.valueDisplay)
+                                .join(", ")}
+                        </Typography>
+                    </StyledWrapper>
+                ))}
+        </ManagedAccordionSection>
+    );
 
     return (
         <Accordion
@@ -80,60 +141,16 @@ const ProductDetailsSpecifications: React.FC<Props> = ({
             data-test-selector="productDetails_specifications"
             {...styles.specificationsAccordion}
         >
-            {renderingDocuments && (
-                <ManagedAccordionSection
-                    title={translate("Documents")}
-                    data-test-selector="productDetails_specifications_documents"
-                    initialExpanded
-                    {...styles.specificationsAccordionSection}
-                >
-                    <GridContainer {...styles.documentsContainer}>
-                        {documents!.map(document => (
-                            <GridItem key={document.id.toString()} {...styles.documentGridItem}>
-                                <Link
-                                    href={document.filePath}
-                                    target="_new"
-                                    {...styles.documentLink}
-                                    data-test-selector={`productDetails_${document.id}`}
-                                >
-                                    <IconMemo {...styles.documentIcon} />
-                                    <Typography {...styles.documentNameText}>{document.name}</Typography>
-                                </Link>
-                            </GridItem>
-                        ))}
-                    </GridContainer>
-                </ManagedAccordionSection>
-            )}
-            {renderingAttributes && (
-                <ManagedAccordionSection
-                    title={translate("Attributes")}
-                    initialExpanded={!renderingDocuments}
-                    data-test-selector="productDetails_specifications_attributes"
-                    {...styles.specificationsAccordionSection}
-                >
-                    {brand && (
-                        <StyledWrapper {...styles.attributeTypeWrapper}>
-                            <Typography {...styles.attributeTypeLabelText}>{translate("Brand")}:&nbsp;</Typography>
-                            <Typography {...styles.attributeTypeValuesText}>{brand.name}</Typography>
-                        </StyledWrapper>
-                    )}
-                    {attributeTypes &&
-                        attributeTypes.map(attributeType => (
-                            <StyledWrapper {...styles.attributeTypeWrapper} key={attributeType.id.toString()}>
-                                <Typography {...styles.attributeTypeLabelText}>{attributeType.label}:&nbsp;</Typography>
-                                <Typography {...styles.attributeTypeValuesText}>
-                                    {attributeType
-                                        .attributeValues!.map(attributeValue => attributeValue.valueDisplay)
-                                        .join(", ")}
-                                </Typography>
-                            </StyledWrapper>
-                        ))}
-                </ManagedAccordionSection>
-            )}
-            {specifications &&
-                specifications.map((specification, index) => (
+            {attributesFirst && attributesAccordion}
+            {documentsFirst && documentsAccordion}
+            {renderingSpecifications &&
+                specifications?.map((specification, index) => (
                     <ManagedAccordionSection
-                        initialExpanded={index === 0 && !renderingDocuments && !renderingAttributes}
+                        initialExpanded={
+                            index === 0 &&
+                            (!documentsFirst || !renderingDocuments) &&
+                            (!attributesFirst || !renderingAttributes)
+                        }
                         key={specification.id.toString()}
                         title={specification.nameDisplay}
                         data-test-selector="productDetails_specifications_specification"
@@ -142,6 +159,8 @@ const ProductDetailsSpecifications: React.FC<Props> = ({
                         {parse(specification.htmlContent, parserOptions)}
                     </ManagedAccordionSection>
                 ))}
+            {!attributesFirst && attributesAccordion}
+            {!documentsFirst && documentsAccordion}
         </Accordion>
     );
 };
@@ -162,12 +181,50 @@ const widgetModule: WidgetModule = {
                 sortOrder: 1,
             },
             {
+                name: fields.documentsPosition,
+                displayName: "Documents Position",
+                editorTemplate: "RadioButtonsField",
+                defaultValue: "first",
+                fieldType: "General",
+                sortOrder: 2,
+                options: [
+                    {
+                        displayName: "Display First",
+                        value: "first",
+                    },
+                    {
+                        displayName: "Display Last",
+                        value: "last",
+                    },
+                ],
+                isVisible: widget => widget.fields.showDocuments,
+            },
+            {
                 name: fields.showAttributes,
                 displayName: "Show Attributes",
                 editorTemplate: "CheckboxField",
                 defaultValue: false,
                 fieldType: "General",
-                sortOrder: 2,
+                sortOrder: 3,
+            },
+            {
+                name: fields.attributesPosition,
+                displayName: "Attributes Position",
+                editorTemplate: "RadioButtonsField",
+                defaultValue: "first",
+                fieldType: "General",
+                sortOrder: 4,
+                options: [
+                    {
+                        displayName: "Display First",
+                        value: "first",
+                    },
+                    {
+                        displayName: "Display Last",
+                        value: "last",
+                    },
+                ],
+                isVisible: widget => widget.fields.showAttributes,
             },
         ],
     },
--- src/InsiteCommerce.Web/Properties/AssemblyInfo.cs
+++ src/InsiteCommerce.Web/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("5.0.6.96")]
-[assembly: AssemblyFileVersion("5.0.6.96")]
+[assembly: AssemblyVersion("5.0.6.103")]
+[assembly: AssemblyFileVersion("5.0.6.103")]
 
