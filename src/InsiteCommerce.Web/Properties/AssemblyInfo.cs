7fca189b35af282488d5f162c38e9e2c496ca253
Insite <insite@insitesoft.com>
2021-05-10 15:52:44 +0000

    5.0.12.566

--- src/InsiteCommerce.Web/Properties/AssemblyInfo.cs
+++ src/InsiteCommerce.Web/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("5.0.12.563")]
-[assembly: AssemblyFileVersion("5.0.12.563")]
+[assembly: AssemblyVersion("5.0.12.566")]
+[assembly: AssemblyFileVersion("5.0.12.566")]
 
